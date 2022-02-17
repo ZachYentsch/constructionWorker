e07e582e7ffb572ee71c24d08577c971c8d686da
Zach <z6chy@icloud.com>
2022-02-17 14:33:05 -0700

    models done

---
+++ Models/Company.cs
@@ -0,0 +1,12 @@
+
+namespace construction.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string CreatorId { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+
+    }
+}
---
+++ Models/Contractor.cs
@@ -0,0 +1,11 @@
+namespace construction.Models
+{
+    public class Contractor
+    {
+        public int Id { get; set; }
+        public string CompanyId { get; set; }
+        public string Name { get; set; }
+        public string Availability { get; set; }
+        public string Speciality { get; set; }
+    }
+}
