da4b25042483d74683c52f2d503ccdd7c3ec619d
Zach <z6chy@icloud.com>
2022-02-17 14:27:31 -0700

    tables done

--- dbSetup.sql
+++ dbSetup.sql
@@ -6,3 +6,20 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+CREATE Table If NOT EXISTS companies(
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  creatorId VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  location VARCHAR(255) NOT NULL,
+  FOREIGN KEY(creatorId) REFERENCES accounts(id)
+) default charset utf8 COMMENT '';
+CREATE Table if NOT EXISTS contractors(
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  companyId Int,
+  name VARCHAR(255) NOT NULL,
+  availablity VARCHAR(255) NOT NULL,
+  specialty VARCHAR (255) NOT NULL,
+  FOREIGN KEY(companyId) REFERENCES companies(id)
+) default charset utf8 COMMENT '';
+drop Table companies;
+drop Table contractors;
